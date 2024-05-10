f6a61a96ab9e00f9483c42d49d76dc67ba3c2696
yasaman <101281998+yasamanBD@users.noreply.github.com>
2024-05-10 23:19:18 +0330

    Add files via upload

---
+++ OS_Lab_EX5/OS_Lab_EX5_server/OS_Lab_EX5_server/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class Program
+{
+    static TcpListener tcpServer;
+
+    public static void Main()
+    {
+        StartListen();
+    }
+
+    public static void StartListen()
+    {
+        try
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            tcpServer = new TcpListener(localAddr, 5000);
+            tcpServer.Start();
+
+            Console.WriteLine("Waiting for a client to connect...");
+
+            // Keep on accepting Client Connection
+            while (true)
+            {
+                TcpClient tcpClient = tcpServer.AcceptTcpClient();
+                Console.WriteLine("Client connected...");
+
+                // New thread for handling each client
+                ThreadPool.QueueUserWorkItem(HandleClient, tcpClient);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public static void HandleClient(Object obj)
+    {
+        var client = (TcpClient)obj;
+        NetworkStream ns = client.GetStream();
+
+        try
+        {
+            using (FileStream fileStream = File.Create("received_file.txt"))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = ns.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fileStream.Write(buffer, 0, bytesRead);
+                }
+            }
+
+            Console.WriteLine("File received and saved successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occurred while receiving or saving the file: " + ex.Message);
+        }
+        finally
+        {
+            ns.Close();
+            client.Close();
+        }
+    }
+
+}
---
+++ OS_Lab_EX5/OS__Lab_EX5/OS__Lab_EX5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+public class TcpClientSample
+{
+    public static void Main()
+    {
+        int port;
+        TcpClient server;
+
+        Console.WriteLine("Please Enter the port number of Server:");
+        port = Int32.Parse(Console.ReadLine());
+        try
+        {
+            server = new TcpClient("127.0.0.1", port);
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Unable to connect to server");
+            return;
+        }
+        Console.WriteLine("Connected to the Server...");
+
+        NetworkStream ns = server.GetStream();
+
+        // Read file content
+        byte[] fileData = File.ReadAllBytes("D:\\Uni\\OS Lab\\OS_Lab_EX5\\test.txt"); // Replace "file_path_here" with the actual path to your file
+
+        // Send file data
+        ns.Write(fileData, 0, fileData.Length);
+        ns.Flush();
+
+        Console.WriteLine("File sent to the server...");
+
+        Console.WriteLine("Disconnecting from server...");
+        ns.Close();
+        server.Close();
+    }
+}
+
